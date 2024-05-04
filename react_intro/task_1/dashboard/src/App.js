45ab218cedf027b79bf2c9a0655592d0e245f7c2
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-03 20:36:08 -0500

    task 1: it's so beautiful, I'm so proud

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,39 @@
+import logo from './atlas-logo.jpg';
+// import icon from './favicon.ico';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="atlas logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;

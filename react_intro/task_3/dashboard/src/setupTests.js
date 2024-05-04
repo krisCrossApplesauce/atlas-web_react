c30e6a24c79434aa044dac2b1a9c9f37ca128087
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-04 16:52:48 -0500

    task 5: interesting that there is apparently no official enzyme adapter for react 18... 16 seems to be the latest one they have (officially)

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
+
+import * as enzyme from 'enzyme';
+import * as Adapter from '@cfaester/enzyme-adapter-react-18';
+
+enzyme.configure({ adapter: new Adapter() });

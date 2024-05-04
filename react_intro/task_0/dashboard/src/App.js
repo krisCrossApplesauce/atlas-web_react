5fa4328da874e053e8a6635e9b05ae1862aa6263
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-03 19:05:18 -0500

    task 0: only issue left is that there's an error that states that the bodycan't exist inside a div, but  the instructions specify a body and due to there being a header and footer as well, it requires an outer div to contain it all otherwise it doesn't work :T

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,37 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </footer>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;

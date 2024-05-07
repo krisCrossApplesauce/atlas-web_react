c191c10789e0683a74bac5318480b4a728927936
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-07 16:26:15 -0500

    need to go eat something so I don't lose all my energy that way I can keep going :P

---
+++ react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import './Header.js';
+import { getFullYear, getFooterCopy } from '../utils/utils.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        {/* <label htmlFor="email">Email </label>
+        <input type="email" id="email" className="email"></input>
+        <label htmlFor="password">Password </label>
+        <input type="password" id="password" className="password"></input>
+        <button>OK</button> */}
+        <form>
+          <div className="inputContainer">
+            <label htmlFor="email">Email </label>
+            <br />
+            <input type="email" id="email" className="email"></input>
+          </div>
+          <div className="inputContainer">
+            <label htmlFor="password">Password </label>
+            <br />
+            <input type="password" id="password" className="password"></input>
+          </div>
+          <button>OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils.js';
+
+function Notifications() {
+    return (
+        <div className="Notifications">
+            <button style={{position: 'absolute', right: '12px'}} aria-label="Close" onClick={() => {console.log('Close button has been clicked')}}>x</button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Notifications;

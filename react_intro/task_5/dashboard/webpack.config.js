6f559a4aef7946c7369d5f614e3518d60013ddab
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-06 21:02:11 -0500

    task 8: that might be good enough? unsure about hot reloading and it says it wants me to make an html file, but I have it in the config file that it'll automatically make one so I don't really care to make one manually, I'll just wait until it tells me what to put in the js file and then I'll run it

---
+++ react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './dist'),
+        },
+        hot: true,
+        compress: true,
+        port: 8564,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gisicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};

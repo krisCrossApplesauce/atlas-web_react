5096060e71c22e57dfc689bfbed7b77799fbc22a
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-09 12:00:14 -0500

    5: food time :P

--- react_props/task_5/dashboard/src/CourseList/CourseList.js
+++ react_props/task_5/dashboard/src/CourseList/CourseList.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import CourseListRow from './CourseListRow';
 import './CourseList.css';
+import PropTypes from 'prop-types';
+import CourseShape from './CourseShape';
 
-function CourseList() {
+function CourseList(props) {
+  let rows;
+  if (props.listCourses == []) {
+    rows = <CourseListRow textFirstCell="No course available yet" />;
+  } else {
+    for (i = 0; i < props.listCourses.length(); i += 1) {
+      // code stuff here, iterate through shape object
+    }
+  }
   return (
     <table id="CourseList">
       <thead>
@@ -10,12 +20,21 @@ function CourseList() {
         <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
       </thead>
       <tbody>
+        {rows}
         <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false} />
         <CourseListRow textFirstCell="Webpack" textSecondCell="20" isHeader={false} />
         <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false} />
       </tbody>
     </table>
   );
+}
+
+CourseList.defaultProps = {
+  listCourses: [],
+}
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
 }
 
 export default CourseList;

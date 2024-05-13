1d0574c9d2e3e6a2d864707667ce01585941fd53
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-12 19:37:33 -0500

    5: ok, this is actually how it should be, I had it the previous way to check how stuff looked :P

--- react_props/task_5/dashboard/src/App/App.js
+++ react_props/task_5/dashboard/src/App/App.js
@@ -5,23 +5,12 @@ import Footer from '../Footer/Footer.js';
 import Login from '../Login/Login.js';
 import Notifications from '../Notifications/Notifications.js';
 import NotificationItemShape from '../Notifications/NotificationItemShape.js';
-import { getLatestNotification } from '../utils/utils.js';
 import CourseList from '../CourseList/CourseList.js';
 import CourseShape from '../CourseList/CourseShape.js';
 import PropTypes from 'prop-types';
 
-const listCourses = [
-  {id: 1, name: 'ES6', credit: 60},
-  {id: 2, name: 'Webpack', credit: 20},
-  {id: 3, name: 'React', credit: 40}
-];
-
-const listNotifications = [
-  {id: 1, type: "default", value: "New course available"},
-  {id: 2, type: "urgent", value: "New resume available"},
-  {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
-];
-
+// replace this with the commented out stuff below to see if everything
+// looks alright with stuff in the notifications and course list stuff
 function App(props) {
   return (
     <>
@@ -29,17 +18,29 @@ function App(props) {
         <div className="header-notifications">
           <Header />
           <div className="root-notifications">
-            <Notifications listNotifications={listNotifications} />
+            <Notifications listNotifications={props.listNotifications} />
           </div>
         </div>
         <div className="App-body">
-          {props.isLoggedIn === false ? (<Login />) : (<CourseList listCourses={listCourses} />)}
+          {props.isLoggedIn === false ? (<Login />) : (<CourseList listCourses={props.listCourses} />)}
         </div>
         <Footer />
       </div>
     </>
   );
 }
+
+// const listCourses = [
+//   {id: 1, name: 'ES6', credit: 60},
+//   {id: 2, name: 'Webpack', credit: 20},
+//   {id: 3, name: 'React', credit: 40}
+// ];
+
+// const listNotifications = [
+//   {id: 1, type: "default", value: "New course available"},
+//   {id: 2, type: "urgent", value: "New resume available"},
+//   {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
+// ];
 
 // function App(props) {
 //   return (
@@ -48,11 +49,11 @@ function App(props) {
 //         <div className="header-notifications">
 //           <Header />
 //           <div className="root-notifications">
-//             <Notifications listNotifications={props.listNotifications} />
+//             <Notifications listNotifications={listNotifications} />
 //           </div>
 //         </div>
 //         <div className="App-body">
-//           {props.isLoggedIn === false ? (<Login />) : (<CourseList listCourses={props.listCourses} />)}
+//           {props.isLoggedIn === false ? (<Login />) : (<CourseList listCourses={listCourses} />)}
 //         </div>
 //         <Footer />
 //       </div>

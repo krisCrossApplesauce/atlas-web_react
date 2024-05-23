911412547d52534af2ef16345718cb589dbf084a
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-22 23:25:40 -0500

    3: it's really not much, but I think that's all I can do before the deadline. I'm just too exhausted to think or focus at this point

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFullYear, getFooterCopy } from '../utils/utils.js';
+import AppContext from '../App/AppContext';
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
     <footer className="App-footer">
       <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      {user.isLoggedIn === true && (
+        <p><a>Contact us</a></p>
+      )}
     </footer>
   );
 }
--- react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -11,3 +11,14 @@ test("tests that Footer at the very least contains the text 'Copyright'", () => 
     const wrapper = shallow(<Footer />);
     expect(wrapper.find('p').text()).toContain('Copyright');
 });
+
+test("tests that the link is not displayed when the user is logged out within the context", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('a')).toHaveLength(0);
+});
+
+test("tests that the link is displayed when the user is logged in within the context", () => {
+    const wrapper = shallow(<Footer />);
+    // something here that makes the context user logged in
+    expect(wrapper.find('a')).toHaveLength(1);
+});

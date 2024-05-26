53955c7c1814096ed6bfaebebfe0af2fb920a1f2
kris_cross_applesauce <karisrr2203@gmail.com>
2024-05-26 15:44:39 -0500

    task 1: was having a hard time figuring out how it wanted me to integrate the entities and normalized data into the function from the last task, but we got there eventually. A little more clarity would've been nice though, the description said to set up a schema by creating 3 entities and then test that it works and I was just like wait what? it's not even a part of my function, what is there to test? Also I feel like resources were hard to find :P

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,19 +1,23 @@
 import * as notificationData from '../../notifications.json';
-import { schema } from 'normalizr';
+import { schema, normalize } from 'normalizr';
 
 const user = new schema.Entity("users");
-const message = new schema.Entity("messages", {
-    idAttribute: "guid",
+const message = new schema.Entity("messages", {}, {
+    idAttribute: 'guid',
 });
 const notification = new schema.Entity("notifications", {
     author: user,
     context: message,
 });
 
+const normalizedData = normalize(notificationData.default, [notification]);
+
 function getAllNotificationsByUser(userId) {
-    return notificationData.default
-        .filter(obj => obj.author.id === userId)
-        .map(obj => obj.context);
+    const userNotifs = Object.values(normalizedData.entities.notifications)
+        .filter(n => n.author === userId)
+        .map(n => normalizedData.entities.messages[n.context]);
+
+    return userNotifs;
 }
 
-export default getAllNotificationsByUser;
+export { getAllNotificationsByUser, normalizedData};

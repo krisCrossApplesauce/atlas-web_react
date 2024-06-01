87ba871869ab0b9bbf993e94a60d53d014348b00
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-01 11:31:07 -0500

    task 1 :)

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App/App.js';
 // import reportWebVitals from './reportWebVitals';
 import './index.css';
+import uiReducer from './reducers/uiReducer.js';
+import { createStore } from 'redux';
+import { Provider } from'react-redux';
+
+const store = createStore(uiReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="container">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </div>
   </React.StrictMode>
 );

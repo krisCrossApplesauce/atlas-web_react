834acc620b2864e76c03bda71dcb1def43037726
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-05 21:50:30 -0500

    task 16 B)

--- react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
@@ -9,12 +9,10 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import { StyleSheetTestUtils } from 'aphrodite';
 import { fromJS } from 'immutable';
-import { createStore } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 // import thunk from 'redux-thunk';
-import uiReducer from '../reducers/uiReducer.js';
-import { Provider } from 'react-redux';
-
-const store = createStore(uiReducer);
+// import rootReducer from '../reducers/rootReducer.js';
+// import { composeWithDevTools } from '@redux-devtools/extension';
 
 global.document = {
     addEventListener: jest.fn(),
@@ -25,109 +23,93 @@ global.window = {
     alert: jest.fn(),
 };
 
-test("tests that App renders without crashing", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toBe(true);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that App contains Notifications", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(Notifications)).toHaveLength(1);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that App contains Header", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.contains(<Header />)).toBe(true);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that App contains Login by default (isLoggedIn = false)", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(Login)).toHaveLength(1);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-test("tests that App does not contain CourseList by default (isLoggedIn = false)", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(CourseList)).toHaveLength(0);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that App contains CourseList when isLoggedIn is true", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.find(CourseList)).toHaveLength(1);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-test("tests that App does not contain Login when isLoggedIn is true", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.find(Login)).toHaveLength(0);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that App contains Footer", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    expect(wrapper.contains(<Footer />)).toBe(true);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-// I cannot get the alertMock part of following test to work for the life of me
-test("tests that App calls the logOut function and the alert function is called with the correct string when the keys control and h are pressed", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    // const alertMock = jest.spyOn(global.window, 'alert').mockImplementation(() => {});
-    const wrapper = shallow(<App />).simulate('keydown', {key: 'h', ctrlKey: true});
-    expect(wrapper.state().user).toEqual({
-        email: '',
-        password: '',
-        isLoggedIn: false,
+describe("tests for App", () => {
+    beforeAll(() => {
+        StyleSheetTestUtils.suppressStyleInjection();
     });
-    // expect(alertMock).toHaveBeenCalledWith('Logging you out');
+    
+    afterAll(() => {
+        StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    });
 
-    // alertMock.mockRestore();
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    it("tests that App renders without crashing", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it("tests that App contains Notifications", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Notifications)).toHaveLength(1);
+    });
+
+    it("tests that App contains Header", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.contains(<Header />)).toBe(true);
+    });
+
+    it("tests that App contains Login by default (isLoggedIn = false)", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Login)).toHaveLength(1);
+    });
+    it("tests that App does not contain CourseList by default (isLoggedIn = false)", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(CourseList)).toHaveLength(0);
+    });
+
+    it("tests that App contains CourseList when isLoggedIn is true", () => {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(CourseList)).toHaveLength(1);
+    });
+    it("tests that App does not contain Login when isLoggedIn is true", () => {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(Login)).toHaveLength(0);
+    });
+
+    it("tests that App contains Footer", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.contains(<Footer />)).toBe(true);
+    });
+
+    // I cannot get the alertMock part of following test to work for the life of me
+    it("tests that App calls the logOut function and the alert function is called with the correct string when the keys control and h are pressed", () => {
+        // const alertMock = jest.spyOn(global.window, 'alert').mockImplementation(() => {});
+        const wrapper = shallow(<App />).simulate('keydown', {key: 'h', ctrlKey: true});
+        expect(wrapper.state().user).toEqual({
+            email: '',
+            password: '',
+            isLoggedIn: false,
+        });
+        // expect(alertMock).toHaveBeenCalledWith('Logging you out');
+
+        // alertMock.mockRestore();
+    });
+
+    // it("tests that App's default state for displayDrawer is false and that it's true after calling handleDisplayDrawer", () => {
+    //     const wrapper = shallow(<App />);
+    //     expect(wrapper.state().displayDrawer).toBe(false);
+
+    //     wrapper.instance().handleDisplayDrawer();
+    //     expect(wrapper.state().displayDrawer).toBe(true);
+    // });
+
+    // it("tests that App's state for displayDrawer is false after calling handleHideDrawer", () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.setState({ displayDrawer: true })
+    //     wrapper.instance().handleHideDrawer();
+    //     expect(wrapper.state().displayDrawer).toBe(false);
+    // });
+
+    it("tests that App's markNotificationAsRead function works correctly", () => {
+        const wrapper = shallow(<App />);
+        wrapper.instance().markNotificationAsRead(2);
+        expect(wrapper.state().listNotifications).toEqual([
+            {id: 1, type: "default", value: "New course available"},
+            {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
+        ]);
+    });
 });
 
-// test("tests that App's default state for displayDrawer is false and that it's true after calling handleDisplayDrawer", () => {
-//     StyleSheetTestUtils.suppressStyleInjection();
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.state().displayDrawer).toBe(false);
-
-//     wrapper.instance().handleDisplayDrawer();
-//     expect(wrapper.state().displayDrawer).toBe(true);
-//     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+// test("tests that mapStateToProps", () => {
+//     let state = fromJS({ isUserLoggedIn: true, isNotificationDrawerVisible: true });
+//     expect(mapStateToProps(state)).toEqual({ isLoggedIn: true, displayDrawer: true });
 // });
-
-// test("tests that App's state for displayDrawer is false after calling handleHideDrawer", () => {
-//     StyleSheetTestUtils.suppressStyleInjection();
-//     const wrapper = shallow(<App />);
-//     wrapper.setState({ displayDrawer: true })
-//     wrapper.instance().handleHideDrawer();
-//     expect(wrapper.state().displayDrawer).toBe(false);
-//     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-// });
-
-test("tests that App's markNotificationAsRead function works correctly", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    const wrapper = shallow(<App />);
-    wrapper.instance().markNotificationAsRead(2);
-    expect(wrapper.state().listNotifications).toEqual([
-        {id: 1, type: "default", value: "New course available"},
-        {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
-    ]);
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-test("tests that mapStateToProps", () => {
-    StyleSheetTestUtils.suppressStyleInjection();
-    let state = fromJS({ isUserLoggedIn: true, isNotificationDrawerVisible: true });
-    expect(mapStateToProps(state)).toEqual({ isLoggedIn: true, displayDrawer: true });
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});

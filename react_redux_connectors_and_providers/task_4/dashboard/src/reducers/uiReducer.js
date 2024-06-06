03300e0821cd45c951052b62a5e44d1aa272dbc0
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-05 21:08:13 -0500

    task 14: combineReducers

--- react_redux_connectors_and_providers/task_4/dashboard/src/reducers/uiReducer.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/reducers/uiReducer.js
@@ -2,36 +2,36 @@ import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, L
 import { Map } from 'immutable';
 
 const initialState = Map({
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {
-        email: '',
-        password: '',
-        isLoggedIn: false,
-    },
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {
+    email: '',
+    password: '',
+    isLoggedIn: false,
+  },
 });
 
 function uiReducer(state = initialState, action) {
-    if (action === undefined) {
-        return state;
-    }
+  if (action === undefined) {
+    return state;
+  }
 
-    switch (action.type) {
-        case DISPLAY_NOTIFICATION_DRAWER:
-            return state.set('isNotificationDrawerVisible', true);
-        case HIDE_NOTIFICATION_DRAWER:
-            return state.set('isNotificationDrawerVisible', false);
-        case LOGIN_SUCCESS:
-            return state.set('isUserLoggedIn', true).set('user', { ...state.get('user'), isLoggedIn: true });
-        case LOGIN_FAILURE:
-            return state.set('isUserLoggedIn', false).set('user', { ...state.get('user'), isLoggedIn: false });
-        case LOGOUT:
-            return state.set('isUserLoggedIn', false).set('user', initialState.get('user'));
-        case LOGIN:
-            return state.set('user', action.user);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case LOGIN_SUCCESS:
+      return state.set('isUserLoggedIn', true).set('user', { ...state.get('user'), isLoggedIn: true });
+    case LOGIN_FAILURE:
+      return state.set('isUserLoggedIn', false).set('user', { ...state.get('user'), isLoggedIn: false });
+    case LOGOUT:
+      return state.set('isUserLoggedIn', false).set('user', initialState.get('user'));
+    case LOGIN:
+      return state.set('user', action.user);
+    default:
+      return state;
+  }
 }
 
 export default uiReducer;
